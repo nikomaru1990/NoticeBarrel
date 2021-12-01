570f9709cc3efce2624b3cd69ef14bb3975b2a3e
nikomaru <76208219+nlkomaru@users.noreply.github.com>
2021-12-02 06:01:20 +0900

    👍Add permission

--- src/main/kotlin/com/noticemc/noticebarrel/commands/CommandManager.kt
+++ src/main/kotlin/com/noticemc/noticebarrel/commands/CommandManager.kt
@@ -1,7 +1,24 @@
+/*
+ * Copyright © 2021 Nikomaru
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.noticemc.noticebarrel.commands
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandPermission
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import java.util.*
@@ -9,11 +26,12 @@ import kotlin.collections.HashMap
 
 class CommandManager : BaseCommand() {
 
+    @CommandPermission("changeBarrel")
     @CommandAlias("cb|changeBarrel")
     fun enableChangeBarrel(sender:CommandSender) {
         val player: Player = sender as Player
         if(canChangeBarrel[player.uniqueId] == null || canChangeBarrel[player.uniqueId] == false) {
-            canChangeBarrel[player.uniqueId] = true;
+            canChangeBarrel[player.uniqueId] = true
             player.sendMessage("ChangeBarrelモードが有効です")
         }else {
             canChangeBarrel[player.uniqueId] = false
